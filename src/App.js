f345a24eb7d1c7475c216f613097bb40bec81eaa
Jin <jin.jin@wclub.co.kr>
2022-06-29 21:52:50 +0900

    Modify: counting modify

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Main from './main';
+import { useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
 
 function App() {
+  const url = 'https://wclub.app/api/v2/schedulers/increase-advertise-count?access_token=T9k6MYKUVdYv9olMTQTSC7t9O6Igx87Q';
+
+  useEffect(() => {
+    axios.post(url).then(() => console.log('count + 1')).catch(error => console.error(error));
+    window.location.replace('https://vo.la/W3f0Kd');
+  }, []);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <main>
+      <img alt="wclub-logo" src="images/logo_transparent.png" />
+     </main>
   );
 }
 
