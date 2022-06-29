9e363a527620125fa06627e4f7a29db8e94eb262
Jin <jin.jin@wclub.co.kr>
2022-06-29 18:55:19 +0900

    Add: W Counting Web

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+import axios from 'axios';
+import './main.css';
+
+const Main = () => {
+  const url = '';
+
+  useEffect(() => {
+    // axios.post(url).then().catch(error => console.error(error));
+    window.location.replace('https://vo.la/W3f0Kd');
+  }, []);
+
+  return (
+  <main>
+    <img src="images/logo_transparent.png" />
+  </main>)
+}
+
+export default Main;
